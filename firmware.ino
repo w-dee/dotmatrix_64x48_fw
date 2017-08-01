019483744be25eb1b8a0c29d77999abd03dbc0da
W.Dee <dee@kikyou.info>
2017-08-01 22:34:16 +0900

    Some fixes

--- firmware.ino
+++ firmware.ino
@@ -15,6 +15,7 @@
 #include "web_server.h"
 #include "wifi.h"
 #include "calendar.h"
+#include "panic.h"
 
 #include "spiffs_api.h"
 extern "C" uint32_t _SPIFFS_start;
@@ -66,7 +67,14 @@ void setup(void){
 
 	FSInfo info;
 
-	SPIFFS.begin();
+	Serial.printf_P(PSTR("Filesystem initialization...\r\n"));
+
+	if(!SPIFFS.begin())
+	{
+		Serial.printf_P(PSTR("Main filesystem mount failed ...\r\n"));
+		Serial.printf_P(PSTR("Should better enter the recovery mode ...\r\n"));
+		// TODO: recovery mode
+	}
 	SPIFFS.info(info);
 	Serial.printf("Main SPIFFS\r\n");
 	Serial.printf("totalBytes    : %d\r\n", info.totalBytes);
@@ -76,7 +84,14 @@ void setup(void){
 	Serial.printf("maxOpenFiles  : %d\r\n", info.maxOpenFiles);
 	Serial.printf("maxPathLength : %d\r\n", info.maxPathLength);
 
-	SETTINGS_SPIFFS.begin();
+	if(!SETTINGS_SPIFFS.begin())
+	{
+		Serial.printf_P(PSTR("Settings filesystem mount failed ...\r\n"));
+		Serial.printf_P(PSTR("Filesystem is now initialized.\r\n"));
+		SETTINGS_SPIFFS.format();
+		if(!SETTINGS_SPIFFS.begin())
+			do_panic(5, F("SPIFFS format failed"));
+	}
 	SETTINGS_SPIFFS.info(info);
 	Serial.printf("Settings SPIFFS\r\n");
 	Serial.printf("totalBytes    : %d\r\n", info.totalBytes);
@@ -86,21 +101,30 @@ void setup(void){
 	Serial.printf("maxOpenFiles  : %d\r\n", info.maxOpenFiles);
 	Serial.printf("maxPathLength : %d\r\n", info.maxPathLength);
 
+
+	Serial.printf_P(PSTR("Infrared initialization...\r\n"));
   ir_init();
+	Serial.printf_P(PSTR("LED matrix driver initialization...\r\n"));
   led_init();
+	Serial.printf_P(PSTR("WiFi initialization...\r\n"));
   wifi_setup();
+	Serial.printf_P(PSTR("Calendar initialization...\r\n"));
   calendar_init();
 
-  Wire.begin(WIRE_SDA, WIRE_SCL);
+ 	Serial.printf_P(PSTR("BME280 initialization...\r\n"));
+ Wire.begin(WIRE_SDA, WIRE_SCL);
 
 
   bme280.begin();
   bme280.setMode(BME280_MODE_NORMAL, BME280_TSB_1000MS, BME280_OSRS_x1, BME280_OSRS_x1, BME280_OSRS_x1, BME280_FILTER_OFF);
 
 
+ 	Serial.printf_P(PSTR("MDNS initialization...\r\n"));
 	  if (MDNS.begin("esp8266")) {
 		Serial.println("MDNS responder started");
 	  }
+
+ 	Serial.printf_P(PSTR("Web server initialization...\r\n"));
 
 	web_server_setup();
 
@@ -110,10 +134,10 @@ void setup(void){
 
 	Serial.printf("\r\n""PortGetFreeHeapSize: %d\r\n", xPortGetFreeHeapSize());
 
-	Serial.printf("begin font init\r\n");
+ 	Serial.printf_P(PSTR("Font initialization...\r\n"));
 	font_bff.begin(BFF_FONT_FILE_START_ADDRESS);
-	Serial.printf("end font init\r\n");
 
+ 	Serial.printf_P(PSTR("UI initialization...\r\n"));
 	ui_setup();
 
 
